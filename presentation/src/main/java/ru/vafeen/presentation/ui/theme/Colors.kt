6c8f08f79934eadc69b69facc6be83b7d7bb21da
A <666av6@gmail.com>
2025-11-04 20:52:17 +0300

    update colors

--- presentation/src/main/java/ru/vafeen/presentation/ui/theme/Colors.kt
+++ presentation/src/main/java/ru/vafeen/presentation/ui/theme/Colors.kt
@@ -2,7 +2,7 @@ package ru.vafeen.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-object Colors {
+internal object Colors {
     val mainLightColor = Color(0xFF68BD45)
     val mainDarkColor = Color(0xFF161617)
     val updateAvailableColor = Color(0xFF0079d6)
